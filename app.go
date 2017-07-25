84d5e8a858c7e7a2d5b3be21f2d9df1a0963745c
fadhilimamk <fadhilimamk@gmail.com>
2017-07-25 16:40:57 +0700

    make init for ambalwarsa more neat

--- app.go
+++ app.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/fadhilimamk/ambalwarsa/src/ambalwarsa"
 	"github.com/fadhilimamk/ambalwarsa/src/conf"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
+var router *gin.Engine
+
 func init() {
-	filename := "./config/config.development.ini"
+
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	formatter := &log.TextFormatter{
+		FullTimestamp: true,
+	}
+	log.SetFormatter(formatter)
+
+	filename := fmt.Sprintf("./config/config.%s.ini", env)
 	if err := conf.InitConfiguration(filename); err != nil {
 		log.Fatal("Error initializing ambalwarsa!")
 	}
 
 	log.WithField("Configuration", conf.Configuration).Info("Config loaded")
+
 }
 
 func main() {
-	router := gin.Default()
+
 	gin.SetMode(conf.Configuration.Server.GINMODE)
+	router = gin.Default()
+	log.Info("Ambalwarsa is listening you ...")
 
 	router.GET("/", ambalwarsa.DefaultHandler)
 
 	router.Run(conf.Configuration.Server.PORT)
+
 }
